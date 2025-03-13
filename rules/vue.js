7e075c9ec410ecb0c0c5681f657f62a093bd107c
Dave Honneffer <pearofducks@gmail.com>
2025-03-13 11:00:18 +0100

    Turn off default prop requirement

--- rules/vue.js
+++ rules/vue.js
@@ -7,15 +7,16 @@ export const vue = defineFlatConfig([
   ...VuePlugin.configs['flat/recommended'],
   {
     rules: {
-    'vue/html-indent': ON,
-    'vue/html-end-tags': ON,
-    'vue/html-self-closing': [ON, { html: { void: 'always' } }],
-    'vue/component-name-in-template-casing': [ON, 'kebab-case'],
-    'vue/attribute-hyphenation': ON,
-    'vue/html-quotes': ON,
-    'vue/no-spaces-around-equal-signs-in-attribute': ON,
-    'vue/no-multi-spaces': ON,
-    'vue/multi-word-component-names': ON
+      'vue/html-indent': ON,
+      'vue/html-end-tags': ON,
+      'vue/html-self-closing': [ON, { html: { void: 'always' } }],
+      'vue/component-name-in-template-casing': [ON, 'kebab-case'],
+      'vue/attribute-hyphenation': ON,
+      'vue/html-quotes': ON,
+      'vue/no-spaces-around-equal-signs-in-attribute': ON,
+      'vue/no-multi-spaces': ON,
+      'vue/multi-word-component-names': ON,
+      'vue/require-default-prop': 'off'
     }
   }
 ])
